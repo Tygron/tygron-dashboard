15b3137135acda3e3953284aacefaf83940532ad
frank <frank@tygron.nl>
2025-06-14 00:12:05 +0200

    tweak collapsible behavior

--- src/js/util/collapsible.js
+++ src/js/util/collapsible.js
@@ -1,25 +1,40 @@
 
 export function initCollapsibles() {
-	
+
 	var coll = document.getElementsByClassName("collapsible");
 	var i;
 
 	for (i = 0; i < coll.length; i++) {
-	  coll[i].addEventListener("click", function() {
-	    this.classList.toggle("active");
-	    var content = this.nextElementSibling;
-	    if (content.style.maxHeight){
-	      content.style.maxHeight = null;
-	    } else {
-	      content.style.maxHeight = content.scrollHeight + "px";
-	    }
-	  });
+		coll[i].addEventListener("click", function() {
+
+			this.classList.toggle("active");
+			var content = this.nextElementSibling;
+							
+			if (content.classList.contains("delayed")) {
+				if (content.style.maxHeight) {
+					content.style.maxHeight = null;
+				} else {
+					content.style.maxHeight = content.scrollHeight + "px";
+				}
+			} else {
+				if (content.style.display === "block") {
+					content.style.display = "none";
+				} else {
+					content.style.display = "block";
+				}
+			}
+
+
+		});
 	}
 }
 
 export function openCollapsibles() {
 	var coll = document.getElementsByClassName("collapsible");
 	for (i = 0; i < coll.length; i++) {
-	  coll[i].click();
+		const collapsible = coll[i];
+		setTimeout(function() {
+			collapsible.click();
+		}, i * 100);
 	}
 }
