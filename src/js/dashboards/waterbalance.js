04244ae9bcf637f22bd80592fd018ca9361c84f0
frank <frank@tygron.nl>
2025-06-14 00:11:29 +0200

    add evaporation m3

--- src/js/dashboards/waterbalance.js
+++ src/js/dashboards/waterbalance.js
@@ -7,6 +7,7 @@ const M3WATER = 'm3Water';
 const M3SEWER = 'm3Sewer';
 const M3STORAGE = 'm3Storage';
 const M3GROUND = 'm3Ground';
+const M3EVAPORATED = "m3Evaporated"
 const TIMEFRAMES = 'timeframes';
 
 const timeframes = 15;
@@ -29,6 +30,7 @@ data[M3WATER] = [112746, 144310, 147322, 149971, 152520, 155013, 157511, 160028,
 data[M3GROUND] = [2316283, 2316850, 2317271, 2317700, 2318122, 2318543, 2318954, 2319384, 2319805, 2320226, 2320647, 2321062, 2321476, 2321886, 2322301];
 data[M3STORAGE] = [3.60, 71.44, 143, 212, 278, 342, 404, 465, 466, 466, 467, 467, 467, 467, 467];
 data[M3SEWER] = [0, 0.01, 0.01, 0.04, 0.01, 0.01, 0.07, 0.02, 0, 0, 0, 0, 0, 0, 0];
+data[M3EVAPORATED] = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140];
 
 
 data[M3LAND] = [];
@@ -46,16 +48,18 @@ titles[TIMEFRAMES] = "Timeframes";
 titles[M3LAND] = "Maaiveld [m3]";
 titles[M3WATER] = "Oppervlaktewater [m3]";
 titles[M3GROUND] = "Groundwater [m3]";
-titles[M3STORAGE] = "Waterbergende voorzieningen";
-titles[M3SEWER] = "Riool";
+titles[M3STORAGE] = "Waterbergende voorzieningen [m3]";
+titles[M3SEWER] = "Riool [m3]";
+titles[M3EVAPORATED] = "Evaporated [m3]";
 
-const properties = [TIMEFRAMES, M3LAND, M3WATER, M3GROUND, M3SEWER, M3STORAGE];
+const properties = [TIMEFRAMES, M3LAND, M3WATER, M3GROUND, M3SEWER, M3STORAGE, M3EVAPORATED];
 const colors = {};
 colors[M3WATER] = [10, 10, 218, 0.5];
 colors[M3LAND] = [10, 218, 10, 0.5];
 colors[M3GROUND] = [165, 42, 42, 0.5];
 colors[M3STORAGE] = [218, 10, 10, 0.5];
 colors[M3SEWER] = [128, 128, 128, 0.5];
+colors[M3EVAPORATED] = [0, 128, 128, 0.5];
 
 initCollapsibles();
 
