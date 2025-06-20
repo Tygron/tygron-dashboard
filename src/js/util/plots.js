0da8958669d675103fa18e2e071eb797e27f6c85
frank <frank@tygron.nl>
2025-06-20 13:33:51 +0200

    make percentual optional, add titles and fix datapoints; Add create layout functions

--- src/js/util/plots.js
+++ src/js/util/plots.js
@@ -1,5 +1,5 @@
 
-export function volumeStackedPlot(plotDivName, data, properties, colors, layout) {
+export function volumeStackedPlot(plotDivName, data, properties, colors, titles, layout, percentual = false) {
 
 	var traces = [];
 	for (let i = 1; i < properties.length; i++) {
@@ -8,30 +8,89 @@ export function volumeStackedPlot(plotDivName, data, properties, colors, layout)
 		series.x = [];
 		series.y = [];
 
+		if (titles != null && titles[properties[i]] != null) {
+			series.name = titles[properties[i]];
+		}
 		series.stackgroup = 'one';
-		if (i == 1) {
+		if (percentual) {
 			series.groupnorm = 'percent';
 		}
 		series.fillcolor = "rgba(" + colors[properties[i]].join(",") + ")";
 
 		for (let t = 0; t < data[properties[i]].length; t++) {
-			series.x.push(data[properties[0]]);
-			series.y.push(data[properties[i]]);
+			series.x.push(data[properties[0]][t]);
+			series.y.push(data[properties[i]][t]);
 		}
 
 		traces.push(series);
 	}
 
 	if (layout == undefined) {
-		layout = {
-
-			title: {
-
-				text: 'Hover on <i>points</i> or <i>fill</i>'
-
-			}
-		};
+		layout = {};
+	}
+	if (layout.title == undefined) {
+		layout.title = {
+			text: percentual ? 'Percentual Volume Stack' : 'Volume Stack'
+		}
 	}
 
 	Plotly.newPlot(plotDivName, traces, layout)
+}
+
+export function createLayout() {
+	/**
+	 * See https://plotly.com/javascript/reference/layout/
+	 */
+	
+	const layout = {
+		title: {
+			automargin: undefined,
+			font: undefined, /*{color, family, lineposition,shadow, size style, textcase, variant, weight}*/
+			pad: undefined,  /*b, l ,r ,t*/
+			subtitle: undefined, /*{
+						font: undefined, 
+						text: undefined		
+						x: undefined,
+						xanchor: undefined,
+						xref: undefined,
+						y: undefined, 
+						yanchor: undefined, 
+						yref: undefined,}*/
+
+			text: undefined,
+			x: undefined,
+			xanchor: undefined,
+			xref: undefined,
+			y: undefined,
+			yanchor: undefined,
+			yref: undefined,
+		},
+		showLegend: undefined,
+
+		legend: {
+			bgcolor: undefined,
+			bordercolor: undefined,
+			borderwidth: undefined,
+			entrywidth: undefined,
+			entrywidthmode: undefined,
+			font: undefined,
+			groupclick: undefined,
+			grouptitlefont: undefined,
+			indentation:undefined,
+			itemclick: undefined,
+			itemdoubleclick: undefined, 
+			itemsizing: undefined,
+		}, //etc
+
+	};
+	return layout;
+}
+
+
+export function createVolumePlotLayout() {
+	const layout = createLayout();
+	/**
+	 * Override specific settings
+	 */
+	return layout;
 }
