62c8257ab5fb6ee8a6266fc787aac567de1fedb3
frank <frank@tygron.nl>
2025-06-23 16:50:00 +0200

    tweak slider properties in slider setup

--- src/js/util/timeframeslider.js
+++ src/js/util/timeframeslider.js
@@ -7,11 +7,15 @@ export function setupTimeframeSlider(timeframeSlider, timeframe, timeframes, onI
 		timeframeSlider.parentElement.style.setProperty('--max', timeframeSlider.max);
 		timeframeSlider.parentElement.style.setProperty('--min', 0);
 		timeframeSlider.parentElement.style.setProperty('--step', 1); /*Compute*/
+		timeframeSlider.parentElement.style.setProperty('--tickEvery', 1);
+		timeframeSlider.parentElement.style.setProperty('--value',timeframeSlider.value); 
+		timeframeSlider.parentElement.style.setProperty('--text-value', JSON.stringify(timeframeSlider.value))	
+		
 
 	}
 	timeframeSlider.oninput = function() {
 		onInput();
-		timeframeSlider.parentNode.style.setProperty('--value',timeframeSlider.value); 
-		timeframeSlider.parentNode.style.setProperty('--text-value', JSON.stringify(timeframeSlider.value))	
+		timeframeSlider.parentElement.style.setProperty('--value',timeframeSlider.value); 
+		timeframeSlider.parentElement.style.setProperty('--text-value', JSON.stringify(timeframeSlider.value))	
 	};
 }
