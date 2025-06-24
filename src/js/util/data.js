b863c8fbc7bfc91cfbeedb56b8ba09d514b429d7
frank <frank@tygron.nl>
2025-06-24 16:43:47 +0200

    move link methods to data script

--- src/js/util/data.js
+++ src/js/util/data.js
@@ -1,49 +1,109 @@
 
 
-export function createTimeframeData(timeframes) {
-	let data = {
-		dataframes: []
-	};
+export function createLinks(properties) {
+	return { properties: properties };
+}
 
-	for (let i = 0; i < timeframes; i++) {
-		data.dataframes.push({});
+export function getLink(links, timeframe) {
+	if (links.timeframeLinks == undefined) {
+		links.timeframeLinks = [];
+	}
+	while (links.timeframeLinks.length - 1 < timeframe) {
+		links.timeframeLinks.push({
+			source: [],
+			target: [],
+			value: [],
+		});
+	}
+	return links.timeframeLinks[timeframe];
+}
+
+export function addLink(links, timeframe, from, to, amount) {
+	let link = getLink(links, timeframe)
+	if (link.source == undefined) {
+		link.source = [];
+	}
+	if (link.target == undefined) {
+		link.target = [];
+	}
+	if (link.value == undefined) {
+		link.value = [];
+	}
+	link.source.push(properties.indexOf(from));
+	link.target.push(properties.indexOf(to));
+	link.value.push(amount);
+}
+
+
+export function createTimeframeData(timeframes, itemID, properties) {
+	let data = {
+		itemID: itemID,
+		timeframes: timeframes
+	};
+	for(let i = 0; i < properties.length; i++){
+		data[properties[i]]= [];
+		for(let j = 0; j < timeframes; j++){
+			data[properties[i]].push(0);
+		}
 	}
 	return data;
 }
 
-export function setTimeframeValues(data, property, values) {
+export function setTimeframeValues(data, property, values, relative = false) {
 
-	for (let i = 0; i < data.dataframes.length && i < values.length; i++) {
-		data.dataframes[i].property[property] = values[i];
+	for (let i = 0; i < data[property].length && i < values.length; i++) {
+		data[property][i] = values[i];
+	}
+
+	if (relative) {
+
+		for (let i = 0; i < data[property].length && i < values.length; i++) {
+			let previous = i == 0 ? 0 : data[property][i - 1];
+			data[property][i] -= previous;
+		}
 	}
 }
 
 export function setTimeframeValue(data, property, value) {
 
 	for (let i = 0; i < data.dataframes.length; i++) {
-		data.dataframes[i].property[property] = value;
+		data[property][i] = value;
 	}
 }
 
-export function addTimeframeValuesForID(data, property, id, idValues, values) {
+export function addFlowValues(data,timeframe, propertyFrom, propertyTo, areaIDFrom, areaIDTo, values, condition=undefined) {
+	if (data[propertyFrom][timeframe] == undefined) {
+		data[propertyFrom][timeframe] = 0;
+	}
+	if (data[propertyTo][timeframe] == undefined) {
+		data[propertyTo][timeframe] = 0;
+	}
 
-	for (let i = 0; i < data.dataframes.length && i < values.length && i < idValues; i++) {
-		if (idValues[i] == id) {
-			if (data.dataframes[i].property[property] == undefined) {
-				data.dataframes[i].property[property] = 0;
+	for (let i = 0; i < values.length && i < areaIDFrom.length && i < areaIDTo.length; i++) {
+		if (areaIDFrom[i] == data.itemID && values[i] != 0 && (condition==undefined || condition[i])) {			
+			if (values[i] > 0) {
+				data[propertyFrom][timeframe] -= values[i];
+			} else {
+				data[propertyFrom][timeframe] += values[i];
 			}
-			data.dataframes[i].property[property] += values[i];
+		}
+		if (areaIDTo[i] == data.itemID && values[i] != 0 && (condition==undefined || condition[i])) {
+			if (values[i] > 0) {
+				data[propertyTo][timeframe] += values[i];
+			} else {
+				data[propertyTo][timeframe] -= values[i];
+			}
 		}
 	}
 }
-export function addValuesForTimeframeAndID(data, timeframe, property, id, idValues, values) {
 
-	let dataframe = data.dataframes[timeframe];
-	if (idValues[i] == id) {
-		if (dataframe.property[property] == undefined) {
-			dataframe.property[property] = 0;
+export function addValuesForTimeframeAndID(data, timeframe, property, idValues, values) {
+
+	if (idValues[i] == data.itemID) {
+		if (data[property][timeframe] == undefined) {
+			data[property][timeframe] = 0;
 		}
-		dataframe.property[property] += values[i];
+		data[property][timeframe] += values[i];
 	}
 
 }
