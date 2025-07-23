b4b4a75378720e2331236c050c828d966d8dcc1b
Tygron <Tygron@users.noreply.github.com>
2025-07-23 15:26:58 +0200

    Added popup function

--- src/js/util/dom.js
+++ src/js/util/dom.js
@@ -13,3 +13,34 @@ export function attachHandler(parentElement, eventType, selector, handler) {
 		parentElement.addEventListener(eventType, checkHandler, false);
 	}
 }
+
+export function popupPanel(element, id, webPath) {
+
+	let popupPanel = document.createElement('div');
+	popupPanel.classList.add('popup');
+	
+	let panelCloser = document.createElement('div');
+	panelCloser.classList.add('closer');
+	attachHandler(panelCloser, 'click', null, function(){clearPopupPanel(element);});
+	
+	let panelFrame = document.createElement('iframe');
+	panelFrame.classList.add('popupFrame');
+	
+	popupPanel.appendChild( panelCloser );
+	popupPanel.appendChild( panelFrame );
+	
+	webPath = webPath ?? '/web/panel.html'
+	let queryString = 'token=$TOKEN&id='+id;
+	let webTarget = webPath + ( webPath.indexOf('?')<0 ? '?' : '&' ) + queryString;
+	panelFrame.src = webTarget;
+	
+	clearPopupPanel(element);
+	element.appendChild(popupPanel);
+}
+export function clearPopupPanel(element) {
+	let popups = element.getElementsByClassName('popup');
+	for ( let i = 0 ; i < popups.length ; i++ ) {
+		popups[i].remove();
+	}
+	return element;
+}
