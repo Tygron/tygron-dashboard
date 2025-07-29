6f685a96566fb137ace9d3783108839e996ef41f
= <frank@tygron.nl>
2025-07-29 15:50:01 +0200

    remove html tags from javascript file

--- src/js/dashboards/profileview.js
+++ src/js/dashboards/profileview.js
@@ -1,32 +1,29 @@
-<script>
-
 //Berging Riool - Berging Land
 const inletGround = [$SELECT_ATTRIBUTE_WHERE_NAME_IS_OBJECT_FLOW_OUTPUT_AND_BUILDING_IS_YA_SEWER_OVERFLOW_AND_TIMEFRAME_IS_X_AND_GRIDTYPE_IS_RAINFALL];
 
-let InletGroundSums = [];
+let inletGroundSums = [];
 
 for (let buildingKey = 0; buildingKey < inletGround.length; buildingKey++) {
 	let buildingValues = inletGround[buildingKey];
 	for (let timeframeKey = 0; timeframeKey < buildingValues.length; timeframeKey++) {
-		InletGroundSums[timeframeKey] = InletGroundSums[timeframeKey] ?? 0;
-		InletGroundSums[timeframeKey] = InletGroundSums[timeframeKey] + buildingValues[timeframeKey];
+		inletGroundSums[timeframeKey] = inletGroundSums[timeframeKey] ? 0 : null;
+		inletGroundSums[timeframeKey] = inletGroundSums[timeframeKey] + buildingValues[timeframeKey];
 	}
 }
 
-console.log (InletGroundSums);
+console.log(inletGroundSums);
 
 
-for (let timeframeKey = 0; timeframeKey < InletGroundSums.length; timeframeKey++) {
+for (let timeframeKey = 0; timeframeKey < inletGroundSums.length; timeframeKey++) {
 	addFlowValues(
 		timeframeKey,
 		OUTLET_GROUND,
 		INLET_GROUND,
 		inletAreaFrom,
 		inletAreaTo,
-		InletGroundSums,
+		inletGroundSums,
 		condition = inletUnderground
 	);
 }
 
-console.log (addFlowValues);
-</script>
+console.log(addFlowValues);
