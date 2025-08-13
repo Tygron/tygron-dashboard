3c6eaac79f7a542f1303a45b3f6bf05d4981678a
Tygron <Tygron@users.noreply.github.com>
2025-08-13 13:48:14 +0200

    Helper to ensure dom element exists. AttachHandler now allows for "this"

--- src/js/util/dom.js
+++ src/js/util/dom.js
@@ -1,16 +1,30 @@
-export function attachHandler(parentElement, eventType, selector, handler) {
+export function ensureDomElement(domElementId) {
+	let domElement = domElementId;
+	if (!(domElement instanceof HTMLElement)) {
+		domElement = document.getElementById(domElementId);
+	}
+	if (!(domElement instanceof HTMLElement)) {
+		throw 'No element found matching '+domElementId;
+	}
+	return domElement;
+}
+
+export function attachHandler(parentElement, eventType, selector, handler, referenceClass) {
 	let checkHandler = function(event) {
 		event = event || window.event;
 		event.target = event.target || event.srcElement;
 		
 		let foundElement = selector ? event.target.closest(selector) : parentElement;
 		if (foundElement) {
-			return handler(foundElement, event);
+			handler.call(foundElement, event);
 		}
 	};
 
 	if (parentElement.addEventListener) {
 		parentElement.addEventListener(eventType, checkHandler, false);
+	}
+	if (referenceClass) {
+		parentElement.classList.add(referenceClass);
 	}
 }
 
