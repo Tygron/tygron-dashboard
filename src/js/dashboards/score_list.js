79f21a73be10e4a3196af145cda294533b3a2a79
Tygron <Tygron@users.noreply.github.com>
2025-08-13 14:32:41 +0200

    Added missing example update

--- src/js/dashboards/score_list.js
+++ src/js/dashboards/score_list.js
@@ -1,23 +1,56 @@
 $(window).on("load", function() {
 
 	let indicatorNames = '$SELECT_NAME_WHERE_INDICATOR_IS_X'.replaceAll('"', '').split(', ');
-	let indicatorActive = [$SELECT_ACTIVE_WHERE_INDICATOR_IS_X];
-	let indicatorScoresCurrent = scaleValues([$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_CURRENT], [0,1], [0,100], true);
-	let indicatorScoresMaquette = scaleValues([$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_MAQUETTE], [0,1], [0,100], true);
+	let indicatorActive = '$SELECT_ACTIVE_WHERE_INDICATOR_IS_X'.replaceAll('"', '').split(', ');
+	let indicatorScoresCurrent = scaleValues([$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_CURRENT], [0, 1], [0, 100], true);
+	let indicatorScoresMaquette = scaleValues([$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_MAQUETTE], [0, 1], [0, 100], true);
 
-	let content = [];
-	content.push( [ '<strong>Indicator</strong>', '<strong>Current</strong>', '<strong>Maquette</strong>' ] );
-	
-	for (let i=0 ; i<indicatorNames.length ; i++) {
-		if (indicatorActive[i] == 0) {
-			continue;
-		}
-		content.push( [ indicatorNames[i], indicatorScoresCurrent[i]+'%', indicatorScoresMaquette[i]+'%' ] );
+
+	/* Listing of all indicators */
+	let listing = new ListingPanelController('listing');
+	listing.addHeader(['Indicator', 'Current', 'Maquette', 'Active']);
+	listing.setDefaultContentRenderTypes(['label', 'label', 'label', { 'type': 'buttons', 'options': ['Include', 'Ignore'] }]);
+
+	for (let i = 0; i < indicatorNames.length; i++) {
+		let activeLabel = (indicatorActive[i] > 0) ? 'Include' : 'Ignore';
+		listing.addContent([indicatorNames[i], indicatorScoresCurrent[i] + '%', indicatorScoresMaquette[i] + '%', activeLabel]);
 	}
+	listing.render();
+
+
+	/* Current and Maquette averages, based on which indicators were active/inactive */
+	let resultContent = []
+	let result = new ListingPanelController('result', { 'flipXY': true });
+	result.addHeader(['Current', 'Maquette']);
+	result.addContent(resultContent);
+
+	let recalculateResult = function(actives) {
+		let count = 0;
+		let current = 0;
+		let maquette = 0;
+		for (let i = 0; i < actives.length; i++) {
+			if (actives[i]) {
+				current += indicatorScoresCurrent[i];
+				maquette += indicatorScoresMaquette[i];
+				count++;
+			}
+		}
+		resultContent[0] = count > 0 ? Math.round(current / count) + '%' : '';
+		resultContent[1] = count > 0 ? Math.round(maquette / count) + '%' : '';
+	}
+	recalculateResult(indicatorActive);
+	result.render();
 	
-	generateListing('listing', content, [
-			getGeneratorEntryLabel(),
-			getGeneratorEntryLabel(),
-			getGeneratorEntryLabel()
-		]);
+
+	/* Use the toggles in the listing to change how the totals are calculated */
+	attachHandler(listing.domElement, 'change', null, function(event) {
+		let values = listing.getValues();
+		for (let i=0;i<values.length;i++) {
+			values[i] = (Array.isArray(values[i]) && values[i].length>0) ? values[i][0] : null;
+			values[i] = (values[i] == 'Include') ? 1 : 0;
+		}
+		recalculateResult(values);
+		result.render();
+	});
+	
 });
