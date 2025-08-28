97df18a3ad4984c1e01ebee4d2de68779f7b3a2e
frank <frank@tygron.nl>
2025-08-28 10:07:51 +0200

    check all timeframes in test

--- test/js/data.js
+++ test/js/data.js
@@ -1,15 +1,17 @@
 import { TimeframeLinks } from './../../src/js/util/data.js';
 
-describe(()=>{
-	
-	it('test timeframe constructor', ()=>{
+describe(() => {
+
+	it('test timeframe constructor', () => {
 		let timeframeProp = "Timeframes";
 		let value1Prop = "Value1";
 		let properties = [timeframeProp, value1Prop];
 		let timeframes = 2;
-		const tfLinks = new TimeframeLinks(properties, timeframes);	
-		
-		let links = tfLinks.getLink(timeframeProp);
-		expect(links).not.toBe(null);
+		const tfLinks = new TimeframeLinks(properties, timeframes);
+
+		for (let i = 0; i < timeframes; i++) {
+			let links = tfLinks.getLink(i);
+			expect(links).not.toBe(null);
+		}
 	});
 });
