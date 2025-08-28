3ad1cd383460b7da8a9da67eb1fc2a689311bad6
frank <frank@tygron.nl>
2025-08-28 10:06:34 +0200

    first test

--- test/js/data.js
+++ test/js/data.js
@@ -1,1 +1,15 @@
+import { TimeframeLinks } from './../../src/js/util/data.js';
 
+describe(()=>{
+	
+	it('test timeframe constructor', ()=>{
+		let timeframeProp = "Timeframes";
+		let value1Prop = "Value1";
+		let properties = [timeframeProp, value1Prop];
+		let timeframes = 2;
+		const tfLinks = new TimeframeLinks(properties, timeframes);	
+		
+		let links = tfLinks.getLink(timeframeProp);
+		expect(links).not.toBe(null);
+	});
+});
