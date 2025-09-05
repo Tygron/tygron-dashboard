4c283c7b79bb22172a4b9088d8c0733283682388
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-09-05 09:25:20 +0200

    Update data.test.js
    first double quote

--- test/js/data.test.js
+++ test/js/data.test.js
@@ -2,7 +2,7 @@ import createLinks from "../src/js/util/data.js"
 
 describe('data ', () => {
   it('has properties', () => {
-    const properties = ["PROP_A", "PROP_B];
+    const properties = ["PROP_A", "PROP_B"];
                         
     const data =createLinksadd(properties);
     expect(data.properties).toBeNotNull();
