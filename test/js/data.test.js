6f171f20e5f0cbf049d19d1240e95ce1b7de2bd6
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-09-05 11:46:23 +0200

    Update data.test.js
    fix not

--- test/js/data.test.js
+++ test/js/data.test.js
@@ -5,13 +5,13 @@ describe('data ', () => {
     const properties = ["PROP_A", "PROP_B"];
                         
     const data =createLinks(properties);
-    expect(data.properties).not().toBeNull();
+    expect(data.properties).not.toBeNull();
   });
 
    it('has empty array of properties', () => {
     const properties = [];
                         
     const data =createLinks(properties);
-    expect(data.properties).not().toBeNull();
+    expect(data.properties).not.toBeNull();
   });
 });
