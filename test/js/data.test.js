af5ae621ff90cd84c3ec1bd28604b147f86b3176
frank <frank@tygron.nl>
2025-09-11 10:47:32 +0200

    format test

--- test/js/data.test.js
+++ test/js/data.test.js
@@ -1,21 +1,23 @@
-import {createLinks} from "./../../src/js/util/data.js"
+import { createLinks } from "./../../src/js/util/data.js"
 
 describe('data ', () => {
-  it('has properties', () => {
-    const properties = ["PROP_A", "PROP_B"];
-                        
-    const data =createLinks(properties);
-    expect(data.properties).not.toBeNull();
-    expect(data.properties).toEqual(
-      expect.arrayOf(expect.any(String)),
-    );
-  });
+	
+	it('has properties', () => {
+		const properties = ["PROP_A", "PROP_B"];
 
-   it('has empty array of properties', () => {
-    const properties = [];
-                        
-    const data =createLinks(properties);
-    expect(data.properties).not.toBeNull();
-    expect(data.properties.length).toEqual(0);
-  });
+		const data = createLinks(properties);
+		expect(data.properties).not.toBeNull();
+		expect(data.properties).toEqual(
+			expect.arrayOf(expect.any(String)),
+		);
+	});
+
+	it('has empty array of properties', () => {
+		const properties = [];
+
+		const data = createLinks(properties);
+		expect(data.properties).not.toBeNull();
+		expect(data.properties.length).toEqual(0);
+	});
+	
 });
