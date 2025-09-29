35ed1344e9503e9ad5d40f90aa63cc68aa75abb0
RudolfTygron <RudolfTygron@users.noreply.github.com>
2025-09-29 12:02:54 +0200

    Rewritten example for legibility, using QueryDataManager

--- dashboards/js/indicator_scores_radarplot.js
+++ dashboards/js/indicator_scores_radarplot.js
@@ -1,34 +1,52 @@
+import { QueryDataManager } from "../util/QueryDataManager.js";
 import { createRadarPlotLayout, createRadarPlot } from "../util/plot.js";
+import { ArrayUtils } from "../util/ArrayUtils.js";
 
 $(window).on("load", function() {
+	let scoreScale = [[0,1],[0,100]];
 
-	let indicatorNames = '$SELECT_NAME_WHERE_INDICATOR_IS_X'.replaceAll('"', '').split(', ');
-	let indicatorActive = [$SELECT_ACTIVE_WHERE_INDICATOR_IS_X];
-	let indicatorScores = {};
-	indicatorScores['Current'] = [$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_CURRENT];
-	indicatorScores['Maquette'] = [$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_MAQUETTE];
-	if (JSON.stringify(indicatorScores['Current']) == JSON.stringify(indicatorScores['Maquette'])) {
-		indicatorScores = { 'Score': indicatorScores['Maquette'] };
+	/* Get all the data from indicators */
+	let queryDataManager = new QueryDataManager();
+	queryDataManager.addQuery('indicatorIds', '$SELECT_ID_WHERE_INDICATOR_IS_X');
+	queryDataManager.addQuery('indicatorNames', '$SELECT_NAME_WHERE_INDICATOR_IS_X');
+	queryDataManager.addQuery('indicatorActive', '$SELECT_ACTIVE_WHERE_INDICATOR_IS_X');
+	queryDataManager.addQuery('indicatorCurrent', '$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_CURRENT');
+	queryDataManager.addQuery('indicatorMaquette', '$SELECT_SCORE_WHERE_INDICATOR_IS_X_AND_MAP_IS_MAQUETTE');
+	
+	/* If the data has not loaded yet, warn the user */
+	if (!queryDataManager.allQueriesResolved()) {
+		document.body.innerHTML = '<p>Recalculation required before data can be shown.</p>';
+		return;
 	}
-
-	let options = [0, 100];
-
-	let labels = [];
-	let values = {};
-
-	for (let key in indicatorScores) {
-		values[key] = [];
+	
+	
+	
+	/* Get all indicator names , filtered to only obtain the names of indicators which are active */
+	let indicatorNames = ArrayUtils.filterByArray( queryDataManager.getData('indicatorNames'), queryDataManager.getData('indicatorActive') );
+	
+	/* Get all indicator scores, filtered to only obtain the scores of indicators which are active */
+	let indicatorScores = {
+	 	'Current': 	ArrayUtils.filterByArray( queryDataManager.getData('indicatorCurrent'), queryDataManager.getData('indicatorActive') ),
+		'Maquette':	ArrayUtils.filterByArray( queryDataManager.getData('indicatorMaquette'), queryDataManager.getData('indicatorActive') ),
+	};
+		
+	/* Scale all the scores to human-readable percentage values */
+	indicatorScores = ArrayUtils.scaleValues(indicatorScores, ...scoreScale, true);
+		
+	/* If Current and Maquette are the same, show only a single set of values */
+	if ( JSON.stringify(indicatorScores['Current']) == JSON.stringify(indicatorScores['Maquette']) ) {
+		indicatorScores = { 'Values' : indicatorScores['Current'] };
 	}
-
-	for (let i = 0; i < indicatorActive.length; i++) {
-		if (indicatorActive[i] > 0) {
-			labels.push(indicatorNames[i]);
-			for (let key in indicatorScores) {
-				let newValue = indicatorScores[key][i] * 100;
-				values[key].push(newValue);
-			}
-		}
-	}
+	
+	
+	/* Establish the values for the radar plot */
+	let radarIndicatorLabels = indicatorNames;
+	let radarIndicatorScoresPerMapType = indicatorScores;
+	let radarRange = scoreScale[1];
+	
+	/* Render the radar plot */
 	let layout = createRadarPlotLayout();
-	createRadarPlot('chart', labels, values, options, layout);
+	createRadarPlot('chart', radarIndicatorLabels, radarIndicatorScoresPerMapType, radarRange, layout);
+	
+
 });
