026f0585cc6fc3d730132e0a22829890f9b83f7b
= <frank@tygron.nl>
2025-11-17 15:08:45 +0100

    format and rename variables

--- src/js/util/Dom.js
+++ src/js/util/Dom.js
@@ -1,15 +1,19 @@
 export function ensureDomElement(domElementId) {
+	
 	let domElement = domElementId;
 	if (!(domElement instanceof HTMLElement)) {
 		domElement = document.getElementById(domElementId);
 	}
+	
 	if (!(domElement instanceof HTMLElement)) {
 		throw 'No element found matching ' + domElementId;
 	}
+	
 	return domElement;
 }
 
 export function attachHandler(parentElement, eventType, selector, handler, referenceClass) {
+	
 	let checkHandler = function(event) {
 		event = event || window.event;
 		event.target = event.target || event.srcElement;
@@ -23,19 +27,20 @@ export function attachHandler(parentElement, eventType, selector, handler, refer
 	if (parentElement.addEventListener) {
 		parentElement.addEventListener(eventType, checkHandler, false);
 	}
+	
 	if (referenceClass) {
 		parentElement.classList.add(referenceClass);
 	}
 }
 
-export function popupPanel(element, id, webPath) {
+export function popupPanel(parent, panelID, webPath) {
 
 	let popupPanel = document.createElement('div');
 	popupPanel.classList.add('popup');
 
 	let panelCloser = document.createElement('div');
 	panelCloser.classList.add('closer');
-	attachHandler(panelCloser, 'click', null, function() { clearPopupPanel(element); });
+	attachHandler(panelCloser, 'click', null, function() { clearPopupPanel(parent); });
 
 	let panelFrame = document.createElement('iframe');
 	panelFrame.classList.add('popupFrame');
@@ -44,13 +49,14 @@ export function popupPanel(element, id, webPath) {
 	popupPanel.appendChild(panelFrame);
 
 	webPath = webPath ?? '/web/panel.html';
-	let queryString = 'token=$TOKEN&id=' + id;
+	let queryString = 'token=$TOKEN&id=' + panelID;
 	let webTarget = webPath + (webPath.indexOf('?') < 0 ? '?' : '&') + queryString;
 	panelFrame.src = webTarget;
 
-	clearPopupPanel(element);
-	element.appendChild(popupPanel);
+	clearPopupPanel(parent);
+	parent.appendChild(popupPanel);
 }
+
 export function clearPopupPanel(element) {
 	let popups = element.getElementsByClassName('popup');
 	for (let i = 0; i < popups.length; i++) {
