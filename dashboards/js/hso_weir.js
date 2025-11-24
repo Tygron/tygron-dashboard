871530a42456fd3515877a00aee0f9a2fb445040
= <frank@tygron.nl>
2025-11-24 16:04:46 +0100

    cleanup hso weir

--- dashboards/js/hso_weir.js
+++ dashboards/js/hso_weir.js
@@ -4,20 +4,12 @@ let weirHeight = 1.5;
 let weirDatumLeft = 2.6;
 let weirDatumRight = 0.4;
 let flow = 1.0;
-let coefficient = 1.1;//sharp
-
-
-//coefficient = 0.865; //broad perpendicular
-//coefficient = 0.91; // board rounded
-//coefficient = 1.3; // rounded
-//coefficient = 1.37; //rounded roof
-//coefficient = 1.23; // custom
 
 let coefficients = [1.1, 0.865, 0.91, 1.3, 1.37, 1.23];
 let names = ["sharp", "broad perpendicular","broad rounded", "rounded", "rounded roof", "custom"];
 
-
 for (let i = 0; i < coefficients.length; i++) {
+	
 	let button = document.createElement("button");
 	const index = i;
 	button.onclick = () =>
@@ -26,8 +18,7 @@ for (let i = 0; i < coefficients.length; i++) {
 	button.innerHTML = names[i];
 	
 	document.body.appendChild(button);
-
 }
 
 let canvas = document.getElementById("weirCanvas");
-visualizeWeir(canvas, weirHeight, weirDatumLeft, weirDatumRight, flow, coefficient);
+visualizeWeir(canvas, weirHeight, weirDatumLeft, weirDatumRight, flow, coefficient[0]);
