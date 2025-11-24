ffaa405c0310eed95663fc607bae8a46cf28e741
= <frank@tygron.nl>
2025-11-24 14:41:43 +0100

    add coefficients for testing

--- dashboards/js/hso_weir.js
+++ dashboards/js/hso_weir.js
@@ -4,6 +4,16 @@ let weirHeight = 1.5;
 let weirDatumLeft= 2.6;
 let weirDatumRight = 0.4;
 let flow = 1.0;
+let coefficient = 1.1;//sharp
+
+
+coefficient = 0.865; //broad perpendicular
+coefficient = 0.91; // board rounded
+//coefficient = 1.3; // rounded
+//coefficient = 1.37; //rounded roof
+//coefficient = 1.23; // custom
+
+
 
 let canvas = document.getElementById("weirCanvas") ;
-visualizeWeir(canvas, weirHeight, weirDatumLeft, weirDatumRight, flow );
+visualizeWeir(canvas, weirHeight, weirDatumLeft, weirDatumRight, flow , coefficient);
