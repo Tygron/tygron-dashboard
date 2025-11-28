340452aee28969e9d8ecaaeb379afe9660a24550
Gebruiker <Gebruiker@DESKTOP-ME9LU06.lan>
2025-11-28 11:02:31 +0100

    Added weir

--- src/js/water/structures/weir.js
+++ src/js/water/structures/weir.js
@@ -280,25 +280,7 @@ export function drawWeirFront(canvas, index, weirHeights, datumsLeft, datumsRigh
 	
 	//TODO: (@ArtistTygron) Implement front view 
 
-	//ctx.beginPath();
-
-	// Set start-point
-	//ctx.moveTo(0,70);
-
-	// Set sub-points
-//	ctx.lineTo(100,70);
-	//ctx.lineTo(100,110);
-	//ctx.lineTo(200,110);
-	//ctx.lineTo(200,70);
-	//ctx.lineTo(300,70);
-	//ctx.lineTo(300,150);
-	//ctx.lineTo(0,150);
 	
-	// Set end-point
-	//ctx.lineTo(0,70);
-
-	// Stroke it (do the drawing)
-	//ctx.stroke();
 	
 	ctx.fillStyle = "grey";
 	ctx.fillRect(0,70, 300,150);
