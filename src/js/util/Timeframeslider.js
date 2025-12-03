26f83349e23c95e28bcdedd23207a968cea62df4
= <frank@tygron.nl>
2025-12-03 16:23:53 +0100

    fix updating properties of timeframe slider

--- src/js/util/Timeframeslider.js
+++ src/js/util/Timeframeslider.js
@@ -1,33 +1,28 @@
 
 
 export function setupTimeframeSlider(timeframeSlider, timeframe, timeframes, onInput) {
-	
-	timeframeSlider.max = timeframes - 1;
-	timeframeSlider.value = timeframe;
 
-	if (timeframeSlider.parentElement != null) {
+	let sliderInput = timeframeSlider.getElementsByTagName('input')[0];
+	if (sliderInput != null) {
+		sliderInput.max = timeframes - 1;
+		sliderInput.value = timeframe;
 
-		let style = timeframeSlider.parentElement.style;
-		
-		style.setProperty('--max', timeframeSlider.max);
-		style.setProperty('--min', 0);
-		style.setProperty('--step', 1);
-		style.setProperty('--tickEvery', 1);
-		style.setProperty('--value', timeframeSlider.value);
-		style.setProperty('--text-value', JSON.stringify(timeframeSlider.value))
+		sliderInput.oninput = function() {
+			onInput();
 
+			timeframeSlider.style.setProperty('--value', sliderInput.value);
+			timeframeSlider.style.setProperty('--text-value', JSON.stringify(sliderInput.value))
 
+		};
 	}
-	
-	timeframeSlider.oninput = function() {
-		onInput();
 
-		if (timeframeSlider.parentElement != null) {
-			
-			let style = timeframeSlider.parentElement.style;
+	timeframeSlider.style.setProperty('--max', sliderInput.max);
+	timeframeSlider.style.setProperty('--min', 0);
+	timeframeSlider.style.setProperty('--step', 1);
+	timeframeSlider.style.setProperty('--tickEvery', 1);
+	timeframeSlider.style.setProperty('--value', sliderInput.value);
+	timeframeSlider.style.setProperty('--text-value', JSON.stringify(sliderInput.value));
 
-			style.setProperty('--value', timeframeSlider.value);
-			style.setProperty('--text-value', JSON.stringify(timeframeSlider.value))
-		}
-	};
 }
+	
+	
