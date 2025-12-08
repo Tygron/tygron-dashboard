404966a6580e3f1949af368221d189ca95d22089
= <frank@tygron.nl>
2025-12-08 11:25:42 +0100

    Add geometry utils

---
+++ src/js/util/GeometryUtils.js
@@ -0,0 +1,16 @@
+
+/**
+* Create a rectangular MultiPolygon object
+*/
+export function createRectangleMP(x, y, width, height) {
+	return { type: "MultiPolygon", 
+		coordinates: [
+			[x, y],
+			[x, y + height],
+			[x + width, y + height],
+			[x + width, y],
+			[x, y]
+		]
+	};
+
+}
