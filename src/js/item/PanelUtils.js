cc688c4b94fb81cd55f7c9039674f17ffe1f84c9
= <frank@tygron.nl>
2025-12-08 14:29:00 +0100

    Add Template panel functions

--- src/js/item/PanelUtils.js
+++ src/js/item/PanelUtils.js
@@ -1,4 +1,4 @@
-import {hasRequiredAttributes} from "./ItemUtils.js"
+import { hasRequiredAttributes } from "./ItemUtils.js"
 
 export function isTemplateTextPanel(panel, templateMapLink, templateAttribute, requiredAttributes) {
 	// Is non template?	
@@ -7,22 +7,58 @@ export function isTemplateTextPanel(panel, templateMapLink, templateAttribute, r
 	}
 
 	// Check template panel type
-	if (panel.excelID != null){
+	if (panel.excelID != null) {
 		return false;
 	}
-	
-	if(templateMapLink!= null && panel.mapLink!= templateMapLink){
+
+	if (templateMapLink != null && panel.mapLink != templateMapLink) {
 		return false;
 	}
-	
-	if(templateAttribute!= null && panel.attribute != templateAttribute){
+
+	if (templateAttribute != null && panel.attribute != templateAttribute) {
 		return false;
 	}
-	
-	if(hasRequiredAttributes!= null && !hasRequiredAttributes(panel,requiredAttributes)){
+
+	if (hasRequiredAttributes != null && !hasRequiredAttributes(panel, requiredAttributes)) {
 		return false;
 	}
-	
+
 	return true;
 
+}
+
+export function getTemplateTextPanel(panels, templateMapLink, templateAttribute, requiredAttributes) {
+
+	if (!Array.isArray(panels)) {
+		return panels;
+	}
+
+	for (let i = 0; i < panels.length; i++) {
+
+		if (isTemplateTextPanel(panels[i], templateMapLink, templateAttribute, requiredAttributes)) {
+			return panels[i];
+		}
+	}
+
+	return null;
+
+
+}
+
+export function getTemplateTextPanels(panels, templateMapLink, templateAttribute, requiredAttributes) {
+
+	let templateTextPanels = [];
+
+	if (!Array.isArray(panels)) {
+		return templateTextPanels;
+	}
+
+	for (let i = 0; i < panels.length; i++) {
+
+		if (isTemplateTextPanel(panels[i], templateMapLink, templateAttribute, requiredAttributes)) {
+			templateTextPanels.push(panels[i]);
+		}
+	}
+
+	return templateTextPanels;
 }
